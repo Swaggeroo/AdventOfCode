56283fd64eef50f89d9fbb2fe43829df1ce86eb2
Swaggeroo <mugel849@gmail.com>
2023-12-01 16:46:05 +0100

    day 1_1

---
+++ src/day1_1.ts
@@ -0,0 +1,29 @@
+import { getDayData } from "./getDayData";
+
+getDayData(2023,1).then((result: string) => {
+    let solution :number = 0;
+
+    for(let i: number = 0; i<result.length; i++){
+        let first: string = "-1";
+        let last: string = "-1";
+        while(result[i] !== "\n"){
+            if(result[i].match(/[0-9]/)){
+                if(first === "-1"){
+                    first = result[i];
+                }else{
+                    last = result[i];
+                }
+            }
+            i++;
+        }
+        if(last === "-1"){
+            last = first;
+        }
+        solution += Number((first + "") + (last + ""));
+    }
+
+    console.log(solution);
+
+});
+
+export {}
