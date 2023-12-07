e1beb91e10049c56cea137089389a85cbdfc353d
Swaggeroo <mugel849@gmail.com>
2023-12-07 10:14:33 +0100

    day 7_2 cleanup

--- src/2023/7/day7_2.ts
+++ src/2023/7/day7_2.ts
@@ -1,111 +1,22 @@
 import { getDayData } from "../../getDayData";
 
 getDayData(2023,7).then((result: string) => {
-    let solultion: number = 0;
-
-
+    let solution: number = 0;
 
     let lines: string[] = result.split('\n');
     lines.pop();
 
-    let hands: Hand[] = [];
+    let hands: Hand[] = lines.map((line: string) => new Hand(line));
 
-    for(let line of lines){
-        let hand: Hand = {
-            cards: line.split(' ')[0],
-            rank: 0,
-            bid: parseInt(line.split(' ')[1])
-        }
-        hands.push(hand);
-    }
-
-    for (let hand of hands){
-        let cards: string[] = hand.cards.split('');
-        let rank: number = 0;
-        let map: Map<string, number> = new Map();
-        for (let card of cards){
-            if(map.has(card)){
-                map.set(card, map.get(card)! + 1);
-            }else{
-                map.set(card, 1);
-            }
-        }
-
-        let j: number = 0;
-        if (map.has('J')){
-            j = map.get('J')!;
-        }
-
-        if (j === 5){
-            rank = Math.max(6, rank);
-        }else if (j === 4){
-            rank = Math.max(5, rank);
-        }else if (j === 3){
-            rank = Math.max(3, rank);
-        }
-
-        for (let [key, value] of map){
-            if (key !== 'J'){
-                if(value + j === 5){
-                    rank = Math.max(6, rank);
-                }else if(value + j === 4){
-                    rank = Math.max(5, rank);
-                }else if(value + j === 3){
-                    rank = Math.max(3, rank);
-                }
-            }
-        }
-
-        if (rank < 4 && (map.size == 2 || (map.size == 3 && j > 0))){
-            rank = 4;
-        }else if (rank < 3){
-            if (j >= 2){
-                rank = Math.max(2, rank);
-            }else{
-                let pairs: number = 0;
-                for (let [key, value] of map){
-                    if(value === 2 && key !== 'J'){
-                        pairs++;
-                    }
-                }
-                if (pairs === 2) {
-                    rank = Math.max(2, rank);
-                }else if (pairs === 1 && j >= 1){
-                    rank = Math.max(2, rank);
-                }else if (pairs === 1){
-                    rank = Math.max(1, rank);
-                }else if (pairs === 0 && j >= 1){
-                    rank = Math.max(1, rank);
-                }
-            }
-        }
-
-        hand.rank = rank;
-    }
-
-    hands.sort((a: Hand, b: Hand) => {
-        if(a.rank !== b.rank){
-            return b.rank - a.rank;
-        }else {
-            for (let i = 0; i < a.cards.length; i++){
-                if (letterToNumber(a.cards[i]) !== letterToNumber(b.cards[i])){
-                    return letterToNumber(b.cards[i]) - letterToNumber(a.cards[i]);
-                }
-            }
-        }
-
-        return 0;
-    });
+    hands.sort(Hand.compare);
 
     let rank = 1;
     for (let i: number = hands.length - 1; i >= 0; i--){
-        let hand: Hand = hands[i];
-        solultion += hand.bid * rank;
+        solution += hands[i].bid * rank;
         rank++;
     }
 
-
-    console.log(solultion);
+    console.log(solution);
 });
 
 function letterToNumber(letter: string): number{
@@ -125,11 +36,78 @@ function letterToNumber(letter: string): number{
     }
 }
 
-interface Hand{
-    cards: string,
-    rank: number,
-    bid: number
-}
+class Hand {
+    cards: string;
+    rank: number;
+    bid: number;
 
+    constructor(line: string) {
+        this.cards = line.split(' ')[0];
+        this.bid = parseInt(line.split(' ')[1]);
+        this.rank = this.getRank(this.cards);
+    }
+
+    private getRank(cardString: string) {
+        let cards: string[] = cardString.split('');
+        let rank: number = 0;
+        let cardCount: Map<string, number> = new Map();
+
+        cards.forEach((card: string) => {
+            cardCount.set(card, cardCount.has(card) ? cardCount.get(card)! + 1 : 1);
+        });
+
+        let jokers: number = cardCount.has('J') ? cardCount.get('J')! : 0;
+
+        let maxCardCount: number = 0;
+        for (let [key, value] of cardCount){
+            if (key !== 'J' && value > maxCardCount){
+                maxCardCount = value;
+            }
+        }
+
+        //Card Multiple same Cards
+        if(maxCardCount + jokers === 5){
+            rank = Math.max(6, rank);
+        }else if(maxCardCount + jokers === 4){
+            rank = Math.max(5, rank);
+        }else if(maxCardCount + jokers === 3){
+            rank = Math.max(3, rank);
+        }
+
+        //Full House and Pairs
+        if (rank < 4 && (cardCount.size == 2 || (cardCount.size == 3 && jokers > 0))){
+            rank = 4;
+        }else if (rank < 3){
+            let pairs: number = 0;
+            for (let [key, value] of cardCount){
+                if(value === 2 && key !== 'J'){
+                    pairs++;
+                }
+            }
+
+            if (pairs === 2 || jokers >= 2 || (pairs === 1 && jokers >= 1)) {
+                rank = Math.max(2, rank);
+            }else if (pairs === 1 || (pairs === 0 && jokers >= 1)){
+                rank = Math.max(1, rank);
+            }
+        }
+
+        return rank;
+    }
+
+    public static compare(a: Hand, b: Hand) {
+        if(a.rank !== b.rank){
+            return b.rank - a.rank;
+        }else {
+            for (let i = 0; i < a.cards.length; i++){
+                if (letterToNumber(a.cards[i]) !== letterToNumber(b.cards[i])){
+                    return letterToNumber(b.cards[i]) - letterToNumber(a.cards[i]);
+                }
+            }
+        }
+
+        return 0;
+    }
+}
 
 export {}
