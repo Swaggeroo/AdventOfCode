ecb7736dc1cd8459f9641b9cfd84ec25b0a91ddf
Swaggeroo <mugel849@gmail.com>
2023-12-09 11:52:58 +0100

    day 9 CLEAN

--- src/2023/9/day9_2.ts
+++ src/2023/9/day9_2.ts
@@ -1,4 +1,4 @@
-import { getDayData } from "../../getDayData";
+import {getDayData} from "../../getDayData";
 
 getDayData(2023, 9).then((result: string) => {
     let lines = result.split('\n');
@@ -10,14 +10,18 @@ getDayData(2023, 9).then((result: string) => {
 
     let dataSets: DataSet[] = [];
 
-    iniDataLines.forEach((dataLine: DataLine) => {dataSets.push(new DataSet(dataLine))});
+    iniDataLines.forEach((dataLine: DataLine) => {
+        dataSets.push(new DataSet(dataLine))
+    });
 
-    dataSets.forEach((dataSet: DataSet) => {dataSet.fillDataLines()});
+    dataSets.forEach((dataSet: DataSet) => {
+        dataSet.fillDataLines()
+    });
 
     console.log(dataSets.reduce((acc: number, dataSet: DataSet) => acc + dataSet.extrapolate(false), 0));
 });
 
-class DataLine{
+class DataLine {
     numbers: number[];
 
     constructor(line: string) {
@@ -25,7 +29,7 @@ class DataLine{
     }
 }
 
-class DataSet{
+class DataSet {
     dataLines: DataLine[] = [];
 
     constructor(dataLines: DataLine) {
@@ -34,28 +38,28 @@ class DataSet{
 
     fillDataLines() {
         let currentLine: DataLine = this.dataLines[0];
-        let newLine: DataLine = {numbers:[]};
+        let newLine: DataLine = {numbers: []};
         do {
-            newLine = {numbers:[]};
-            for (let j = 0; j <= currentLine.numbers.length-2; j++) {
-                newLine.numbers.push(currentLine.numbers[j+1] - currentLine.numbers[j]);
+            newLine = {numbers: []};
+            for (let j = 0; j <= currentLine.numbers.length - 2; j++) {
+                newLine.numbers.push(currentLine.numbers[j + 1] - currentLine.numbers[j]);
             }
             currentLine = newLine;
             this.dataLines.push(newLine);
-        }while (!newLine.numbers.every((number: number) => number === 0));
+        } while (!newLine.numbers.every((number: number) => number === 0));
     }
 
-    extrapolate(right: boolean): number{
+    extrapolate(right: boolean): number {
         let nextNumber: number = 0;
-        for (let j = this.dataLines.length-2; j >= 0; j--) {
-            if (right){
-                nextNumber += this.dataLines[j].numbers[this.dataLines[j].numbers.length-1];
-            }else {
+        for (let j = this.dataLines.length - 2; j >= 0; j--) {
+            if (right) {
+                nextNumber += this.dataLines[j].numbers[this.dataLines[j].numbers.length - 1];
+            } else {
                 nextNumber = this.dataLines[j].numbers[0] - nextNumber;
             }
         }
 
-        return  nextNumber;
+        return nextNumber;
     }
 }
 
